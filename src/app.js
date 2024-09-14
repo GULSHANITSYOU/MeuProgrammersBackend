b2b196dcc0f7dbbd064d848c83678b434050fef2
GUSLHAN KUMAR <gulshanitsyou@gmail.com>
2024-09-15 01:15:04 +0530

    Add seted up app.js file

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import { corsOptions } from "./constants.js";
+import cors from "cors";
+
+//=========== express app =======
+const app = express();
+
+//======== cors middelware ======
+app.use(cors(corsOptions));
+
+//======== json parser =========
+app.use(express.json({ limit: "20kb" }));
+
+//======== url encoded ========
+app.use(express.urlencoded({ extended: true, limit: "20kb" }));
+
+//======== cookieparser ========
+app.use(cookieParser());
+
+//========= Middlewares for Media (🙄) =======
+app.use(express.static("public"));
+
+export { app };
