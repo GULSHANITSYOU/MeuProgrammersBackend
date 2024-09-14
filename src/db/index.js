6d400634d40366173cab3ad44078f533f6c21263
GUSLHAN KUMAR <gulshanitsyou@gmail.com>
2024-09-14 19:13:03 +0530

    Add new Auther in pacage.json

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { DbName } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DbName}`
+    );
+
+    console.log(
+      `\n mongoDB is connected! at Host : ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log(`mongoDB connection is Faild : ${error}`);
+    process.exit(1);
+  }
+};
+
+export default { connectDB };
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import { connectDB } from "./db/index.js";
+dotenv.config({ path: "/.env" });
+import { app } from "./app.js";
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Hurray!! app is litening at PORT : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log(`MongoDb connection faild ${err}`));
