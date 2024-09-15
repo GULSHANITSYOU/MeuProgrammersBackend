d9a3e4a7accbd764cee3925c726bf35f3d483463
GUSLHAN KUMAR <gulshanitsyou@gmail.com>
2024-09-16 03:45:29 +0530

    Add time stamp in club model

--- src/models/club.model.js
+++ src/models/club.model.js
@@ -1,58 +1,61 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
-const clubSchema = new Schema({
-  clubName: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  clubTitle: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  clubDescription: {
-    type: String,
-    required: true,
-  },
-  clubLogo: {
-    type: String,
-    required: true,
-  },
-  clubPoster: {
-    type: String,
-  },
-  clubVideos: [
-    {
+const clubSchema = new Schema(
+  {
+    clubName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    clubTitle: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    clubDescription: {
+      type: String,
+      required: true,
+    },
+    clubLogo: {
+      type: String,
+      required: true,
+    },
+    clubPoster: {
       type: String,
     },
-  ],
-  clubImages: [
-    {
-      type: String,
+    clubVideos: [
+      {
+        type: String,
+      },
+    ],
+    clubImages: [
+      {
+        type: String,
+      },
+    ],
+    clubPresidentId: {
+      type: Schema.Types.ObjectId,
+      ref: "Student",
     },
-  ],
-  clubPresidentId: {
-    type: Schema.Types.ObjectId,
-    ref: "Student",
+    clubMembers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Student",
+      },
+    ],
+    teamMembersCore: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Student",
+      },
+    ],
+    clubMembers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Student",
+      },
+    ],
   },
-  clubMembers: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Student",
-    },
-  ],
-  teamMembersCore: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Student",
-    },
-  ],
-  clubMembers: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Student",
-    },
-  ],
-});
+  { timestamps: true }
+);
 export const Club = mongoose.model("Club", clubSchema);
