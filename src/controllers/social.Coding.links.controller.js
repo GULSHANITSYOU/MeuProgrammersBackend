fbc2b0258412f0e254992040b608ca57a34dc0ab
Abhinav Dhiman <166062355+AbhinavDhiman34@users.noreply.github.com>
2024-09-29 03:18:15 +0530

    feat: created a feature to add/ update the coding profiles

--- src/controllers/social.Coding.links.controller.js
+++ src/controllers/social.Coding.links.controller.js
@@ -2,6 +2,7 @@ import { asyncHandler } from "../utils/async.handler.js";
 import { SocialProfile } from "../models/socialprofile.model.js";
 import { apiResponse } from "../utils/api.response.js";
 import { apiError } from "../utils/api.error.js";
+import { CodingProfile } from "../models/codingprofile.model.js";
 
 async function updateprofilelinks(req, res) {
   const student = req.student;
@@ -29,7 +30,7 @@ async function updateprofilelinks(req, res) {
     );
 }
 
-const handelSocialCodingLinks = asyncHandler(async (req, res) => {
+const handelSocialLinks = asyncHandler(async (req, res) => {
   const student = req.student;
 
   const { linkedin, github, twitterX, instagram } = req.body;
@@ -56,4 +57,84 @@ const handelSocialCodingLinks = asyncHandler(async (req, res) => {
     );
 });
 
-export { handelSocialCodingLinks };
+async function updatecodingprofile(req, res) {
+  const student = req.student;
+  const {
+    leetcode,
+    geeksForGeeks,
+    codeChef,
+    codeForces,
+    atCoder,
+    hackerRank,
+    hackerEarth,
+    interviewBit,
+  } = req.body;
+
+  const codingprofile = await CodingProfile.findByIdAndUpdate(
+    student.CodingProfile,
+    {
+      leetcode,
+      geeksForGeeks,
+      codeChef,
+      codeForces,
+      atCoder,
+      hackerRank,
+      hackerEarth,
+      interviewBit,
+    },
+    { runValidators: true, new: true }
+  );
+
+  if (!codingprofile) {
+    throw new apiError("fail", 500, "can not update coding profile links");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new apiResponse("success", 201, codingprofile, "Links added Successfully")
+    );
+}
+const handelCodingLinks = asyncHandler(async (req, res) => {
+  const student = req.student;
+  if (student.CodingProfile) {
+    return await updatecodingprofile(req, res);
+  }
+
+  const {
+    leetcode,
+    geeksForGeeks,
+    codeChef,
+    codeForces,
+    atCoder,
+    hackerRank,
+    hackerEarth,
+    interviewBit,
+  } = req.body;
+
+  const newcodingprofile = new CodingProfile({
+    studentId: student._id,
+    leetcode: leetcode || "",
+    geeksForGeeks: geeksForGeeks || "",
+    codeChef: codeChef || "",
+    codeForces: codeForces || "",
+    atCoder: atCoder || "",
+    hackerRank: hackerRank || "",
+    hackerEarth: hackerEarth || "",
+    interviewBit: interviewBit || "",
+  });
+  const codingprofile = await newcodingprofile.save();
+  student.CodingProfile = newcodingprofile._id;
+  await student.save();
+  return res
+    .status(201)
+    .json(
+      new apiResponse(
+        "success",
+        201,
+        codingprofile,
+        "Successfully added coding profiles"
+      )
+    );
+});
+export { handelSocialLinks, handelCodingLinks };
--- src/routes/social.coding.links.route.js
+++ src/routes/social.coding.links.route.js
@@ -1,11 +1,20 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/auth.middelware.js";
-import { handelSocialCodingLinks } from "../controllers/social.Coding.links.controller.js";
+import {
+  handelSocialLinks,
+  handelCodingLinks,
+} from "../controllers/social.Coding.links.controller.js";
 
 const socialCodingLinksRoute = Router();
 
+// route for changing or updating social profile links
 socialCodingLinksRoute
   .route("/add/social-Links")
-  .post(verifyToken, handelSocialCodingLinks);
+  .post(verifyToken, handelSocialLinks);
+
+// route for changing or updating  coding profile links
+socialCodingLinksRoute
+  .route("/add/Coding-Links")
+  .post(verifyToken, handelCodingLinks);
 
 export default socialCodingLinksRoute;
