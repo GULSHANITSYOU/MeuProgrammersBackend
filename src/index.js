e79f333d84838c3454b58f6758ad0316d6a95819
GUSLHAN KUMAR <gulshanitsyou@gmail.com>
2024-09-30 00:48:34 +0530

    port change

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { app } from "./app.js";
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    app.listen(process.env.PORT || 3000, () => {
       console.log(`Hurray!! app is litening at PORT : ${process.env.PORT}`);
     });
   })
